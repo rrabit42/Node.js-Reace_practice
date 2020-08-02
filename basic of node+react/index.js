d31ec42b24255bf51a579977c09d3d42443e86a2
rrabit42 <gegiraffe@gmail.com>
2020-08-02 19:42:40 +0900

    [basic] connect with mongodb using mongoose

--- basic of node+react/index.js
+++ basic of node+react/index.js
@@ -2,6 +2,16 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const mongoose = require('mongoose') //mongoose: 몽고db를 편하게 사용할 수 있게 해주는 툴, npm install mongoose --save
+mongoose.connect('mongodb+srv://<id>:<password>@basic.sbk7x.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
