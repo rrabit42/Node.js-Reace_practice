fd71ef5cf68baf62c3039fbd60e6e14df58149e3
rrabit42 <gegiraffe@gmail.com>
2020-08-05 01:01:44 +0900

    [basic] login & token generate with Bcrypt, jwonwebtoken

--- basic of node+react/index.js
+++ basic of node+react/index.js
@@ -2,17 +2,20 @@ const express = require('express')
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const config = require('./config/key');
 
 const { User } = require("./models/User");
 
-const mongoose = require('mongoose') //mongoose: 몽고db를 편하게 사용할 수 있게 해주는 툴, npm install mongoose --save
+const mongoose = require('mongoose'); //mongoose: 몽고db를 편하게 사용할 수 있게 해주는 툴, npm install mongoose --save
+const { json } = require('body-parser');
 
 // application/x-www-form-urlencoded 타입 데이터 분석해서 가져옴
 app.use(bodyParser.urlencoded({extended:true}));
 // application/json 타입 데이터 분석해서 가져옴
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 mongoose.connect(config.mongoURI, {
     useNewUrlParser: true,
@@ -44,6 +47,45 @@ app.post('/register', (req, res) => {
     })
     return res.status(200).json({
       suceess: true
+    })
+  })
+})
+
+app.post('/login', (req, res) => {
+  // 요청된 이메일을 데이터베이스에서 있는지 찾는다.
+  // mongodb 제공 method
+  User.findOne({email: req.body.email}, (err, user) => {
+    if(!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다."
+      })
+    }
+
+    // 요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인
+    // comparePassword 함수의 인자로는 pw과 cb(callback)함수
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if(!isMatch)
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 틀렸습니다."
+        })
+
+      // 비밀번호까지 맞다면 토큰을 생성하기
+      // generateToken 함수의 인자로 cb뿐
+      user.generateToken((err, user) => {
+        if(err) return res.status(400).send(err);
+        
+        // 토큰을 저장한다. 어디에 ? 쿠키, 로컬스토리지, 세션...
+        // 우리는 쿠키에다 할거임!
+        // 이름이 x_auth란 쿠키가 생성됨
+        res.cookie("x_auth", user.token)
+        .status(200)
+        .json({
+          loginSuccess: true,
+          userId: user._id
+        })
+      })
     })
   })
 })
--- basic of node+react/models/User.js
+++ basic of node+react/models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10   // salt가 몇글자 인지. 즉 10자리인 salt
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
   name: {
@@ -59,6 +60,38 @@ userSchema.pre('save', function(next){
     next()
   }
 });
+
+// comparePassword 라는 이름의 함수는 pw와 cb을 인자로 가진다.
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+  // plainPassword와 암호화된 비밀번호 hash값 체크
+  // hash는 복호화가 불가능하기 때문에 plainPassword도 암호화 후 비교
+  bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+    if(err) return cb(err)
+    cb(null, isMatch) //에러 없고, isMatch에 true가 들어 있음
+  })
+}
+
+// gegnerateToken 이름의 함수는 cb만 인자로 가진다.
+// 걍 함수 선언법이라고 생각해!
+userSchema.methods.generateToken = function(cb) {
+  var user = this;
+
+  //jsonwebtoken을 이용해서 token을 생성하기
+  //_id: mongodb collection에서 확인해봐, 그리고 sign에 인자에는 String으로 들어가야해서 변환해줌!
+  /*
+    이 함수는
+    user._id + 'secretToken' = token을 발급한다.
+    그리고 나중에 secretToken을 넣으면 user._id를 알 수 있게 됨!
+    따라서 secretToken은 기억해줘야함!
+  */
+  var token = jwt.sign(user._id.toHexString(), 'secretToken')
+
+  user.token = token
+  user.save(function(err, user) {
+    if(err) return cb(err)
+    cb(null, user)
+  })
+}
 
 // 인자로는 (이 모델의 이름, 스키마)
 const User = mongoose.model('User', userSchema)
