047a7a3e360f4806c3fd291db849c0027f6b162b
rrabit42 <gegiraffe@gmail.com>
2020-08-08 02:40:17 +0900

    [basic] React - React Router Dom

--- basic of node+react/client/src/App.js
+++ basic of node+react/client/src/App.js
@@ -1,12 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import LandingPage from './components/views/LandingPage/LandingPage'
+import LoginPage from './components/views/LoginPage/LoginPage'
+import RegisterPage from './components/views/RegisterPage/RegisterPage'
+
 
 function App() {
   return (
-    <div className="App">
-      AppAppApp
-    </div>
+    <Router>
+      <div>
+
+        <hr />
+
+        {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        <Switch>
+          {/*
+          기존 코드
+            <Route path="/">
+              <LoginPage />
+            </Route>
+          를 한줄로 적는 방법
+          */}
+          <Route exact path="/" component = {LandingPage} />
+          <Route exact path="/login" component = {LoginPage} />
+          <Route exact path="/register" component = {RegisterPage} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
