4e836541d1df3ac4c8fa49b1610075834a63b9ad
rrabit42 <gegiraffe@gmail.com>
2020-08-08 15:20:42 +0900

    [basic] client & backend - data flow & axios

--- basic of node+react/client/src/components/views/LandingPage/LandingPage.js
+++ basic of node+react/client/src/components/views/LandingPage/LandingPage.js
@@ -1,6 +1,13 @@
 import React from 'react'
 
 function LandingPage() {
+
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/hello') // get request를 서버에 보낸다.
+    .then(response => console.log(response.data)) // response 데이터를 콘솔창에 보여준다.
+  }, [])
+
   return (
     <div>
       LandingPage
